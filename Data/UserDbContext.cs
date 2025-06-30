2d6a22d97398d4e89e5abbe8eeeb6df8cf5db367
Shruti Pancholi <pancholishruti8@gmail.com>
2025-06-30 14:25:38 +0530

    DataBase Upload

---
+++ Data/UserDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JwtEx.Data;
+using JwtEx.Model;
+
+namespace JwtEx.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
