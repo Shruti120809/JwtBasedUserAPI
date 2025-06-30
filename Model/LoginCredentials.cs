cdc49fa4ba50b9b886e5de1f10c23a3fa417e718
Shruti Pancholi <pancholishruti8@gmail.com>
2025-06-30 14:27:14 +0530

    Schema Uploaded

---
+++ Model/LoginCredentials.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JwtEx.Model;
+
+public class LoginCredentials
+{
+
+    [Required]
+    [EmailAddress(ErrorMessage = "Enter valid Email Id")]
+    public string email { get; set; }
+
+    [Required]
+    public string password { get; set; }
+}
---
+++ Model/User.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JwtEx.Model
+{
+    public class User
+    {
+        [Key]
+        [JsonIgnore]
+        public int Id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        [EmailAddress (ErrorMessage = "Enter valid Email Id")]
+        public string email { get; set; }
+
+        [Required]
+        public string password { get; set; }
+
+        [NotMapped]
+        [Compare("password", ErrorMessage = "Passwords do not match")]
+        public string confirmPassword { get; set; }
+
+    }
+}
