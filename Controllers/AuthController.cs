e6b6dc028361f9cd9aec9fa1418ed357fa6e7745
Shruti Pancholi <pancholishruti8@gmail.com>
2025-06-30 14:24:53 +0530

    controllers uplod

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,97 @@
+﻿using JwtEx.Data;
+using JwtEx.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtEx.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserDbContext _userdbcontext;
+        private readonly IConfiguration _config;
+
+        public AuthController(IConfiguration config, UserDbContext userDbContext)
+        {
+            _config = config;
+            _userdbcontext = userDbContext;
+
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(User cred)
+        {
+            var existingUser = _userdbcontext.Users.Any(u => u.email == cred.email);
+            if (existingUser) 
+            {
+                return BadRequest("Email already exists");
+            }
+
+            var hasher = new PasswordHasher<User>();
+            var user = new User
+            {
+                name = cred.name,
+                email = cred.email,
+                password = cred.password
+            };
+
+            user.password = hasher.HashPassword(user, cred.password);
+            user.confirmPassword = user.password;
+
+            _userdbcontext.Users.Add(user);
+            _userdbcontext.SaveChanges();
+            return Ok("User registered successfully");          
+        }
+
+
+        [HttpPost("Login")]
+        public IActionResult Login(LoginCredentials cred)
+        {
+            var user = _userdbcontext.Users.FirstOrDefault(u => u.email == cred.email);
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            var hasher = new PasswordHasher<User>();
+            var result = hasher.VerifyHashedPassword(user, user.password, cred.password);
+            if (result == PasswordVerificationResult.Failed)
+            {
+                return Unauthorized("Invalid credentials");
+            }
+
+            var token = GenerateToken(user);
+            return Ok(new { token });
+        }
+
+        private string GenerateToken(User user)
+        {
+            var jwt = _config.GetSection("JwtConfig");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.name),  // Add this
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email,user.email),
+                new Claim(ClaimTypes.Role, "User")
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: jwt["Issuer"],
+                audience: jwt["Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: creds
+             );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,135 @@
+﻿using JwtEx.Data;
+using JwtEx.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+
+namespace JwtEx.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfileController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+        public ProfileController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        private int GetUserIdFromToken()
+        {
+            var idStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return int.TryParse(idStr, out int Id) ? Id : 0;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("Greet")]
+        public IActionResult Greet()
+        {
+            var userName = User.Identity.Name;
+            return Ok($"Hello {userName}, you're authenticated");
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("GetProfile")]
+        
+        public IActionResult GetProfile()
+        {
+
+            var userId = GetUserIdFromToken();
+            if (userId == 0)
+                return Unauthorized("Invalid token or user not found");
+
+            // Get user based on Id
+            var user = _context.Users
+                .Where(x => x.Id == userId)
+                .Select(x => new UserDTO
+                {
+                    Id = x.Id,
+                    Name = x.name,
+                    Email = x.email
+                })
+                .FirstOrDefault();
+
+            if (user == null)
+                return NotFound("User not found");
+
+            return Ok(user);
+        }
+
+        [Authorize]
+        [HttpPut]
+        [Route("Update")]
+        
+        public IActionResult UpdateProfile([FromBody] UserDTO model)
+        {
+            var userId = GetUserIdFromToken();
+
+            if (userId == 0)
+                return Unauthorized("Invalid token or user not found");
+
+            var existingUser = _context.Users.Where(x => x.Id == userId).FirstOrDefault();
+
+            existingUser.name = model.Name;
+            existingUser.email = model.Email;
+
+            _context.SaveChanges();
+            return Ok("Detail Updated");
+        }
+
+        [Authorize]
+        [HttpPatch]
+        [Route("UpdatePartial")]
+        public ActionResult UpdateUserPartial(string email, [FromBody] JsonPatchDocument<User> patchDocument)
+        {
+            if (patchDocument == null)
+                BadRequest();
+
+            var existingUser = _context.Users.Where(s => s.email == email).FirstOrDefault();
+
+            if (existingUser == null)
+                return NotFound();
+
+            var user = new User
+            {
+                Id = existingUser.Id,
+                name = existingUser.name,
+                email = existingUser.email,
+                password = existingUser.password
+            };
+
+            patchDocument.ApplyTo(user, ModelState);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            existingUser.name = user.name;
+            existingUser.email = user.email;
+
+            _context.SaveChanges();
+
+            return Ok("Detail Updated");
+        }
+
+        [HttpDelete]
+        [Route ("Delete")]
+        public IActionResult DeleteProfile()
+        {
+            var userId = GetUserIdFromToken();
+
+            if (userId == 0)
+                return Unauthorized("Invalid token or user not found");
+
+            var existingUser = _context.Users.Where(x => x.Id == userId).FirstOrDefault();
+
+            _context.Remove(existingUser);
+            _context.SaveChanges();
+            return Ok("User Delete");
+        }
+    }
+}
